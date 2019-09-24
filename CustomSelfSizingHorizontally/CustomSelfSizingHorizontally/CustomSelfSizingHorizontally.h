c6a605e6dfacdd8208011e11a2371262277a665c
Kuba Kadyrbekov <k.kadyrbekov95@gmail.com>
2019-09-24 16:57:34 +0600

    added framework

---
+++ CustomSelfSizingHorizontally/CustomSelfSizingHorizontally/CustomSelfSizingHorizontally.h
@@ -0,0 +1,19 @@
+//
+//  CustomSelfSizingHorizontally.h
+//  CustomSelfSizingHorizontally
+//
+//  Created by KubaRahmetMobile on 9/24/19.
+//  Copyright © 2019 kuba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CustomSelfSizingHorizontally.
+FOUNDATION_EXPORT double CustomSelfSizingHorizontallyVersionNumber;
+
+//! Project version string for CustomSelfSizingHorizontally.
+FOUNDATION_EXPORT const unsigned char CustomSelfSizingHorizontallyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomSelfSizingHorizontally/PublicHeader.h>
+
+
